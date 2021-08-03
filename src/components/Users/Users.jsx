0bd65b5ed311171be814b67b2be5e85c60009abe
Administrator <Purplejamkiev@gmail.com>
2021-08-04 00:36:30 +0300

    add fetch

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,22 +3,31 @@ import css from './Users.module.css';
 
 
 const Users = (props) => {
-debugger;
+
+
+
 if (props.users.length === 0) {
+	let result =	fetch('https://social-network.samuraijs.com/api/1.0/users')
+		.then(response => response.json())
+		.then(json => props.setusers(json.items));
+}
+
+
+/* if (props.users.length === 0) {
 	props.setusers( [
         {id: 1, fotoUrl: "https://i.pravatar.cc/50", followed: true, fullName: 'Dmitry', status: 'Boss', location: {city: 'Minsk', country: 'Belarus'} },
         {id: 2, fotoUrl: "https://i.pravatar.cc/50", followed: true, fullName: 'Dmitry', status: 'Boss', location: {city: 'Moscow', country: 'Russia'} },
         {id: 3, fotoUrl: "https://i.pravatar.cc/50", followed: false, fullName: 'Dmitry', status: 'Boss', location: {city: 'Kiev', country: 'Ukraine'} },
         {id: 4, fotoUrl: "https://i.pravatar.cc/50", followed: true, fullName: 'Dmitry', status: 'Boss', location: {city: 'Minsk', country: 'Belarus'} }
     ])
-}
+} */
 
 
 	return (
-		props.users.map(u => <div key={u.id}>
+		props.users.map(u => <div key={"u.id"}>
 			<span>
 				<div>
-					<img src={u.fotoUrl}/>
+					<img src={"u.fotoUrl"}/>
 				</div>
 			</span>
 
@@ -29,12 +38,12 @@ if (props.users.length === 0) {
 			</div>
 			<span>
 				<span>
-					<div>{u.fullName}</div>
+					<div>{u.name}</div>
 					<div>{u.status}</div>
 				</span>
 				<span>
-					<div>{u.location.country}</div>
-					<div>{u.location.city}</div>
+					<div>{"u.location.country"}</div>
+					<div>{"u.location.city"}</div>
 				</span>
 			</span>
 
