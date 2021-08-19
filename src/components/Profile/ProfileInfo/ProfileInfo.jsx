080f308f6afe42f95af25076ffc1316076146726
Administrator <Purplejamkiev@gmail.com>
2021-08-19 14:41:31 +0300

    profile with hooks

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './ProfileInfo.module.css';
 import preloader from './../../common/Preloader.js';
 import ProfileStatus from './ProfileStatus.jsx';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks.jsx';
 
 
 
@@ -16,7 +17,8 @@ const ProfileInfo = (props) => {
             <div><img className={classes.banner}src="https://images.freeimages.com/images/large-previews/bd7/falloxbow-1058032.jpg" alt=""></img>
             </div>
             {/* <div><img src={props.profile.photos.large}/></div> */}
-            <ProfileStatus status={props.status} updateProfileStatus={props.updateProfileStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateProfileStatus={props.updateProfileStatus}/>
+            {/* <ProfileStatus status={props.status} updateProfileStatus={props.updateProfileStatus}/> */}
             <div className={classes.decriptionBlock}>
                 {props.profile.aboutMe}
             </div>
--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './ProfileInfo.module.css';
 
 
@@ -7,11 +7,27 @@ import classes from './ProfileInfo.module.css';
 
 const ProfileStatusWithHooks = (props) => {
 
+const [editMode, editModeSet] = useState(false);
+const [status, setStatus] = useState(props.status);
+
+const editModeOn = () => {
+    editModeSet(true);
+}
+
+const editModeOff = () => {
+    editModeSet(false);
+}
+
+const statusChange = (e) => {
+    let value = e.target.value;
+    setStatus(value);
+    props.updateProfileStatus(value);  
+}
 
     return (<div>
-                {true 
-                    ? <div onDoubleClick={this.editModeOn}><span>{this.props.status || 'Enter status'}</span></div>
-                    : <div onBlur={this.editModeOff}><input onChange={this.statusChange} autoFocus value={this.state.status}/></div>}
+                {!editMode 
+                    ? <div onDoubleClick={editModeOn}><span>{props.status || 'Enter status'}</span></div>
+                    : <div onBlur={editModeOff}><input onChange={statusChange} autoFocus value={status}/></div>}
 
             </div>)
 
